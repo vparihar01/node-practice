65412154ade387fb0b968baa0204bc5fd9de40dd
Vivek Parihar <vivek@weboniselab.com>
2013-10-15 15:00:53 +0530

    request.pie done

--- 3.js
+++ 3.js
@@ -19,6 +19,7 @@ server.on("request",function(request,response){
 
 //##### Printin back request data to as reponse on browser#####//
 server.on("request", function(request, response){
+	console.log("I am inside the reposnse.write");
 	response.writeHead(200);
 	request.on('data',function(chunk){	
 		response.write("writing back data using the write" + chunk + "\n");
@@ -30,12 +31,13 @@ server.on("request", function(request, response){
 
 //##### Printin back request data to as reponse on browser using the pipe#####//
 server.on("request",function(request,response){
+	console.log("I am inside the request.pise");
 	response.writeHead(200);
 	request.pipe(response);
 	request.on('end', function(){
 		response.end();
 	});
-});
+}).listen(8080);
 
  server.on("close",function(){
  
